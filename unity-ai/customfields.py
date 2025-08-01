f3dcace24f0d71dd1b22855efc1bdbe68313ba5f
cyrusparsons <cyrus.parsons@gov.bc.ca>
2025-08-01 22:33:55 +0000

    continuing to experiment with custom fields

--- unity-ai/customfields.py
+++ unity-ai/customfields.py
@@ -1,19 +1,34 @@
-import main
 import os
 import json
 import dotenv
 import textwrap
+import requests
 
 dotenv.load_dotenv()
+headers = {"x-api-key": os.getenv("METABASE_KEY")}
+
+def get_sql(sql, db_id, metabase_url):
+    ds_req = {
+        "database": db_id,
+        "type": "native",
+        "native": {"query": sql},
+    }
+    r = requests.post(
+        f"{metabase_url}/api/dataset",
+        headers=headers,
+        json=ds_req,
+    )
+    r.raise_for_status()
+    return r.json()["data"]
 
 def get_worksheets():
     sql = f'SELECT "Worksheets"."Name", "Worksheets"."Id" FROM "Flex"."Worksheets"'
-    data = main.get_sql(sql, 3, os.getenv("MB_URL"))
+    data = get_sql(sql, 3, os.getenv("MB_URL"))
     return [{"name": r[0], "id": r[1]} for r in data["rows"]]
 
 def get_worksheet_instances(worksheetId):
     sql = f'SELECT "WorksheetInstances"."CurrentValue", "WorksheetInstances"."WorksheetCorrelationId" FROM "Flex"."WorksheetInstances" WHERE "WorksheetInstances"."WorksheetId" = \'{worksheetId}\''
-    data = main.get_sql(sql, 3, os.getenv("MB_URL"))
+    data = get_sql(sql, 3, os.getenv("MB_URL"))
     return data
 
 def get_custom_labels():
@@ -28,7 +43,7 @@ def get_custom_labels():
         ORDER BY "CustomFields"."Key"
         LIMIT {batch_size} OFFSET {offset}
         '''
-        rows = main.get_sql(sql, 3, os.getenv("MB_URL"))["rows"]
+        rows = get_sql(sql, 3, os.getenv("MB_URL"))["rows"]
 
         if not rows:
             break
@@ -67,10 +82,11 @@ _(none)_
     "    ")}
         '''
             parsed.append(parsed_string)
+            break
     return parsed
 
 
-print(get_worksheets())
+print(get_parsed_worksheets())
 
 '''
 **Extraction pattern (PostgreSQL)**
@@ -80,4 +96,46 @@ SELECT  wi."Id" AS WorksheetInstanceId,
 FROM    Flex."WorksheetInstances"  wi
 CROSS   JOIN LATERAL jsonb_array_elements(wi."CurrentValue" -> 'values') elem
 GROUP BY wi."Id"
+
+Note: in worksheetinstances table: WorksheetCorrelationId, WorksheetId, ID
+Virtual: Name: customLabel1, customLabel2,.. applicationId 
+
+SELECT count(customLabel1) FROM public."Name" WHERE customLabel2 = 'field2'
+
+SELECT COUNT(t."customLabel1")
+FROM (
+  SELECT
+    wi."Id",
+    MAX(elem->>'value') FILTER (WHERE elem->>'key' = 'customLabel1') AS "customLabel1",
+    MAX(elem->>'value') FILTER (WHERE elem->>'key' = 'customLabel2') AS "customLabel2"
+  FROM "Flex"."WorksheetInstances" wi
+  CROSS JOIN LATERAL jsonb_array_elements(wi."CurrentValue"->'values') elem
+  GROUP BY wi."Id"
+) AS t
+WHERE t."customLabel2" = 'field2';
+
+See for example:
+WITH applicant AS (
+  SELECT
+      wi."Id" AS "WorksheetInstanceId",
+      MAX(CASE WHEN elem->>'key' = 'leadApplicant' THEN elem->>'value' END)           AS "leadApplicant",
+      MAX(CASE WHEN elem->>'key' = 'projectIntendedOutcomes' THEN elem->>'value' END) AS "projectIntendedOutcomes"
+  FROM "Flex"."WorksheetInstances" AS wi
+  CROSS JOIN LATERAL jsonb_array_elements(wi."CurrentValue"->'values') AS elem
+  GROUP BY wi."Id"
+)
+SELECT
+    a."projectIntendedOutcomes",
+    app."SubmissionDate"
+FROM "Flex"."WorksheetInstances" AS wi
+JOIN applicant AS a
+  ON wi."Id" = a."WorksheetInstanceId"
+JOIN "public"."Applications" AS app
+  ON app."Id" = wi."CorrelationId"
+WHERE a."leadApplicant" = 'Local Government';
+
+
+May be impossible to programmatically convert virtual table to required SQL 
+Talk to Andre about this, maybe create views/tables for each worksheet?
+
 '''
